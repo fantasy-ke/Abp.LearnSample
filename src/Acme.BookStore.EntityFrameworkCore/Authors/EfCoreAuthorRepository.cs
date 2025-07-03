b3e7169d5a265c3e44c669b6cc5e7eb456ab2be3
fantasy-ke <2246080525@qq.com>
2025-07-03 18:45:33 +0800

    1

---
+++ src/Acme.BookStore.EntityFrameworkCore/Authors/EfCoreAuthorRepository.cs
@@ -0,0 +1,43 @@
+﻿using Acme.BookStore.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace Acme.BookStore.Authors;
+
+public class AuthorRepository : EfCoreRepository<BookStoreDbContext, Author, Guid>, IAuthorRepository
+{
+    public AuthorRepository(
+       IDbContextProvider<BookStoreDbContext> dbContextProvider
+       ) : base(dbContextProvider)
+    {
+    }
+    public async Task<Author> FindByNameAsync(string name)
+    {
+        var queryable = await GetQueryableAsync();
+        return await queryable.FirstOrDefaultAsync(author => author.Name == name)!;
+    }
+
+    public async Task<List<Author>> GetListAsync(
+        int skipCount,
+        int maxResultCount,
+        string sorting,
+        string filter = null)
+    {
+        var queryable = await GetQueryableAsync();
+        return await queryable
+            .WhereIf<Author, IQueryable<Author>>(
+                !filter.IsNullOrWhiteSpace(),
+                author => author.Name.Contains(filter)
+            )
+            //.OrderBy(sorting)
+            .As<IQueryable<Author>>()
+            .Skip(skipCount)
+            .Take(maxResultCount)
+            .ToListAsync();
+    }
+}
