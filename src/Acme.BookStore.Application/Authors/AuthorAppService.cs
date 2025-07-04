224d9447d35148f0e817ee1ad2def045359e317f
fantasy-ke <2246080525@qq.com>
2025-07-04 09:45:39 +0800

    第 8 部分：作者：应用程序层

---
+++ src/Acme.BookStore.Application/Authors/AuthorAppService.cs
@@ -0,0 +1,85 @@
+﻿using Acme.BookStore.Permissions;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore.Authors
+{
+    [Authorize(BookStorePermissions.Authors.Default)]
+    public class AuthorAppService : BookStoreAppService, IAuthorAppService
+    {
+        readonly IAuthorRepository _authorRepository;
+        readonly AuthorManager _authorManager;
+        public AuthorAppService(AuthorManager authorManager, IAuthorRepository authorRepository)
+        {
+            _authorManager = authorManager;
+            _authorRepository = authorRepository;
+        }
+
+        [Authorize(BookStorePermissions.Authors.Create)]
+        public async Task<AuthorDto> CreateAsync(CreateAuthorDto input)
+        {
+            var author = await _authorManager.CreateAsync(
+                input.Name,
+                input.BirthDate,
+                input.ShortBio
+            );
+
+            await _authorRepository.InsertAsync(author);
+            return ObjectMapper.Map<Author, AuthorDto>(author);
+        }
+
+        [Authorize(BookStorePermissions.Authors.Delete)]
+        public Task DeleteAsync(Guid id)
+        {
+            return _authorRepository.DeleteAsync(id);
+        }
+
+        public async Task<AuthorDto> GetAsync(Guid id)
+        {
+            var author = await _authorRepository.GetAsync(id);
+            return ObjectMapper.Map<Author, AuthorDto>(author);
+        }
+
+        public async Task<PagedResultDto<AuthorDto>> GetListAsync(GetAuthorListDto input)
+        {
+            if (input.Sorting.IsNullOrWhiteSpace())
+            {
+                input.Sorting = nameof(Author.Name);
+            }
+
+           var authors = await  _authorRepository.GetListAsync(
+                input.SkipCount,
+                input.MaxResultCount,
+                input.Sorting,
+                input.Filter
+            );
+            var totalCount = input.Filter == null ? await _authorRepository.GetCountAsync():
+                                await _authorRepository.CountAsync(author => author.Name.Contains(input.Filter));
+
+            return new PagedResultDto<AuthorDto>(totalCount,
+                ObjectMapper.Map<List<Author>, List<AuthorDto>>(authors));
+        }
+
+        [Authorize(BookStorePermissions.Authors.Edit)]
+        public async Task UpdateAsync(Guid id, UpdateAuthorDto input)
+        {
+           var author = await _authorRepository.GetAsync(id);
+
+            if (author.Name != input.Name)
+            {
+                await _authorManager.ChangeNameAsync(author, input.Name);
+            }
+
+            author.BirthDate = input.BirthDate;
+            author.ShortBio = input.ShortBio;
+
+            await _authorRepository.UpdateAsync(author);
+        }
+    }
+}
