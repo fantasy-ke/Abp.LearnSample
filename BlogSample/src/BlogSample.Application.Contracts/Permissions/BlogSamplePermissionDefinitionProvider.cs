a8759753f7cf175447bd8eb82007619517805ed4
fantasy-ke <2246080525@qq.com>
2025-07-06 14:20:41 +0800

    feat(权限管理): 添加博客系统相关权限
    - 在 BlogSamplePermissionDefinitionProvider 中添加了博客、帖子、标签和评论相关的权限
    - 在 BlogSamplePermissions 中定义了新的权限常量，并添加了获取所有权限的方法

--- BlogSample/src/BlogSample.Application.Contracts/Permissions/BlogSamplePermissionDefinitionProvider.cs
+++ BlogSample/src/BlogSample.Application.Contracts/Permissions/BlogSamplePermissionDefinitionProvider.cs
@@ -11,6 +11,27 @@ public class BlogSamplePermissionDefinitionProvider : PermissionDefinitionProvid
     {
         var myGroup = context.AddGroup(BlogSamplePermissions.GroupName);
 
+        var blogs = myGroup.AddPermission(BlogSamplePermissions.Blogs.Default, L("Permission:Blogs"));
+        blogs.AddChild(BlogSamplePermissions.Blogs.Management, L("Permission:Management"));
+        blogs.AddChild(BlogSamplePermissions.Blogs.Update, L("Permission:Edit"));
+        blogs.AddChild(BlogSamplePermissions.Blogs.Delete, L("Permission:Delete"));
+        blogs.AddChild(BlogSamplePermissions.Blogs.Create, L("Permission:Create"));
+        blogs.AddChild(BlogSamplePermissions.Blogs.ClearCache, L("Permission:ClearCache"));
+
+        var posts = myGroup.AddPermission(BlogSamplePermissions.Posts.Default, L("Permission:Posts"));
+        posts.AddChild(BlogSamplePermissions.Posts.Update, L("Permission:Edit"));
+        posts.AddChild(BlogSamplePermissions.Posts.Delete, L("Permission:Delete"));
+        posts.AddChild(BlogSamplePermissions.Posts.Create, L("Permission:Create"));
+
+        var tags = myGroup.AddPermission(BlogSamplePermissions.Tags.Default, L("Permission:Tags"));
+        tags.AddChild(BlogSamplePermissions.Tags.Update, L("Permission:Edit"));
+        tags.AddChild(BlogSamplePermissions.Tags.Delete, L("Permission:Delete"));
+        tags.AddChild(BlogSamplePermissions.Tags.Create, L("Permission:Create"));
+
+        var comments = myGroup.AddPermission(BlogSamplePermissions.Comments.Default, L("Permission:Comments"));
+        comments.AddChild(BlogSamplePermissions.Comments.Update, L("Permission:Edit"));
+        comments.AddChild(BlogSamplePermissions.Comments.Delete, L("Permission:Delete"));
+        comments.AddChild(BlogSamplePermissions.Comments.Create, L("Permission:Create"));
         //Define your own permissions here. Example:
         //myGroup.AddPermission(BlogSamplePermissions.MyPermission1, L("Permission:MyPermission1"));
     }
--- BlogSample/src/BlogSample.Application.Contracts/Permissions/BlogSamplePermissions.cs
+++ BlogSample/src/BlogSample.Application.Contracts/Permissions/BlogSamplePermissions.cs
@@ -1,3 +1,5 @@
+using Volo.Abp.Reflection;
+
 namespace BlogSample.Permissions;
 
 public static class BlogSamplePermissions
@@ -5,6 +7,44 @@ public static class BlogSamplePermissions
     public const string GroupName = "BlogSample";
 
 
+    public static class Blogs
+    {
+        public const string Default = GroupName + ".Blog";
+        public const string Management = Default + ".Management";
+        public const string Delete = Default + ".Delete";
+        public const string Update = Default + ".Update";
+        public const string Create = Default + ".Create";
+        public const string ClearCache = Default + ".ClearCache";
+    }
+
+    public static class Posts
+    {
+        public const string Default = GroupName + ".Post";
+        public const string Delete = Default + ".Delete";
+        public const string Update = Default + ".Update";
+        public const string Create = Default + ".Create";
+    }
+
+    public static class Tags
+    {
+        public const string Default = GroupName + ".Tag";
+        public const string Delete = Default + ".Delete";
+        public const string Update = Default + ".Update";
+        public const string Create = Default + ".Create";
+    }
+
+    public static class Comments
+    {
+        public const string Default = GroupName + ".Comment";
+        public const string Delete = Default + ".Delete";
+        public const string Update = Default + ".Update";
+        public const string Create = Default + ".Create";
+    }
+
+    public static string[] GetAll()
+    {
+        return ReflectionHelper.GetPublicConstantsRecursively(typeof(BlogSamplePermissions));
+    }
     
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";
