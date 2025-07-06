f5b8d20579cb564ede8d77ba2aa05c0bca656b09
fantasy-ke <2246080525@qq.com>
2025-07-06 12:35:22 +0800

    feat(comment): 添加评论模块的数据访问层接口和实现
    - 新增 ICommentRepository 接口，定义了评论模块所需的各种数据操作方法
    - 实现 CommentRepository 类，提供对评论数据的实际数据库操作- 添加了获取评论列表、获取评论数量、获取回复评论和删除评论等功能

---
+++ BlogSample/src/BlogSample.Domain/Comments/ICommentRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+
+namespace BlogSample.Comments;
+
+public interface ICommentRepository: IRepository<Comment, Guid>
+{
+    /// <summary>
+    /// 根据文章Id 获取评论
+    /// </summary>
+    /// <param name="postId"></param>
+    /// <returns></returns>
+    Task<List<Comment>> GetListOfPostAsync(Guid postId);
+    /// <summary>
+    /// 根据文章Id 获取评论数量
+    /// </summary>
+    /// <param name="postId"></param>
+    /// <returns></returns>
+    Task<int> GetCommentCountOfPostAsync(Guid postId);
+    /// <summary>
+    /// 根据评论Id 下面的子获取评论
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    Task<List<Comment>> GetRepliesOfComment(Guid id);
+
+    Task DeleteOfPost(Guid id);
+}
---
+++ BlogSample/src/BlogSample.EntityFrameworkCore/Comments/CommentRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogSample.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace BlogSample.Comments;
+
+public class CommentRepository:EfCoreRepository<BlogSampleDbContext, Comment, Guid>,ICommentRepository
+{
+    public CommentRepository(IDbContextProvider<BlogSampleDbContext> dbContextProvider) : base(dbContextProvider)
+    {
+    }
+
+    public async Task<List<Comment>> GetListOfPostAsync(Guid postId)
+    {
+        return await (await GetDbSetAsync())
+            .Where(a => a.PostId == postId)
+            .OrderBy(a => a.CreationTime)
+            .ToListAsync();
+    }
+
+    public async Task<int> GetCommentCountOfPostAsync(Guid postId)
+    {
+        return await (await GetDbSetAsync())
+            .CountAsync(a => a.PostId == postId);
+    }
+
+    public async Task<List<Comment>> GetRepliesOfComment(Guid id)
+    {
+        return await (await GetDbSetAsync())
+            .Where(a => a.RepliedCommentId == id).ToListAsync();
+    }
+
+    public async Task DeleteOfPost(Guid id)
+    {
+        var recordsToDelete = await (await GetDbSetAsync()).Where(pt => pt.PostId == id).ToListAsync();
+        (await GetDbSetAsync()).RemoveRange(recordsToDelete);
+    }
+}
